5791f4f25bbae0b254307d0af59699876124d439
Joseph <underradicals@gmail.com>
2025-07-16 12:50:26 -0500

    Clean-up / Refactor
    On branch schemas
    Your branch is up to date with 'origin/schemas'.
    
    Changes to be committed:
    	modified:   load.py
    	renamed:    sql/copy_from_table.sql -> sql/copy_data_from_world_conent.sql
    	new file:   sql/copy_data_from_world_content.sql
    	renamed:    sql/tables.sql -> sql/create_world_content_tables.sql
    	renamed:    sql/drop_tables.sql -> sql/drop_world_content_tables.sql

--- load.py
+++ load.py
@@ -18,54 +18,75 @@ create_table_names = '\n'.join([f"create table if not exists {x.split(":")[0]} (
                               x in
                               _keys_list])
 def create_tables_sql():
-    with open(f'{ROOT}\\sql\\tables.sql', 'w', encoding='utf-8') as f:
+    with open(f'{ROOT}\\sql\\create_world_content_tables.sql', 'w', encoding='utf-8') as f:
         f.write(create_table_names)
 
 def drop_tables_sql():
-    with open(f'{ROOT}\\sql\\drop_tables.sql', 'w', encoding='utf-8') as f:
+    with open(f'{ROOT}\\sql\\drop_world_content_tables.sql', 'w', encoding='utf-8') as f:
         f.write(drop_table_names)
 
 
 def copy_from_sql():
-    with open(f'{ROOT}\\sql\\copy_from_table.sql', 'w', encoding='utf-8') as f:
+    with open(f'{ROOT}\\sql\\copy_data_from_world_content.sql', 'w', encoding='utf-8') as f:
         copy_from_query = '\n'.join([f"COPY {x} (id, json) FROM '{ROOT}\\csv\\{x}.csv' WITH (FORMAT csv,HEADER true,QUOTE \'\"\',ESCAPE \'\"\');" for x in keys_list])
         f.write(copy_from_query)
+
+
+def drop_world_content_tables(conn):
+    with conn.cursor() as cur:
+        logging.info(f"Dropping tables")
+        cur.execute(drop_table_names)
+
+
+def create_world_content_tables(conn):
+    with conn.cursor() as cur:
+        logging.info(f"Creating tables")
+        cur.execute(create_table_names)
+
+
+def to_dict(table_name: str):
+    json_blob = str(r.get(f"{table_name}:jwccp"))
+    return orjson.loads(json_blob.encode('utf-8'))
+
+
+def create_manifest_schema_csv_files(table_name: str, json_dict: dict):
+    with open(f'{ROOT}\\csv\\{table_name}.csv', 'w', encoding='utf-8', newline='') as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_MINIMAL)
+        for key, value in json_dict.items():
+            writer.writerow([int(key), orjson.dumps(value).decode('utf-8')])
+
+
+def seed_world_content_database(conn):
+    with conn.cursor() as cur:
+        for table_name in keys_list:
+            logging.info(f"COPY FROM {table_name}.csv")
+            cur.execute(f"""
+                COPY {table_name} (id, json)
+                FROM '{ROOT}\\csv\\{table_name}.csv'
+                WITH (
+                    FORMAT csv,
+                    HEADER true,
+                    QUOTE '"',
+                    ESCAPE '"'
+                )
+            """)
+
 
 def copy_from():
     with ConnectionPool(conninfo='host=localhost port=5432 dbname=world_content user=postgres password=postgres') as pool:
         with pool.connection() as conn:
-            with conn.cursor() as cur:
-                logging.info(f"Dropping tables")
-                cur.execute(drop_table_names)
+            drop_world_content_tables(conn)
 
         with pool.connection() as conn:
-            with conn.cursor() as cur:
-                logging.info(f"Creating tables")
-                cur.execute(create_table_names)
+            create_world_content_tables(conn)
 
         for table_name in keys_list:
             logging.info(f"Creating {table_name}.csv")
-            json_blob = str(r.get(f"{table_name}:jwccp"))
-            json_dict = orjson.loads(json_blob.encode('utf-8'))
-            with open(f'{ROOT}\\csv\\{table_name}.csv', 'w', encoding='utf-8', newline='') as f:
-                writer = csv.writer(f, quoting=csv.QUOTE_MINIMAL)
-                for key, value in json_dict.items():
-                    writer.writerow([int(key), orjson.dumps(value).decode('utf-8')])
+            json_dict = to_dict(table_name)
+            create_manifest_schema_csv_files(table_name, json_dict)
 
         with pool.connection() as conn:
-            with conn.cursor() as cur:
-                for table_name in keys_list:
-                    logging.info(f"COPY FROM {table_name}.csv")
-                    cur.execute(f"""
-                        COPY {table_name} (id, json)
-                        FROM '{ROOT}\\csv\\{table_name}.csv'
-                        WITH (
-                            FORMAT csv,
-                            HEADER true,
-                            QUOTE '"',
-                            ESCAPE '"'
-                        )
-                    """)
+            seed_world_content_database(conn)
             conn.commit()
 
 def load_main():
